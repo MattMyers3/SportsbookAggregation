6bf89384efbc97b7bdc3be0b1bbac1f9c5bca1f2
Jordan Franklin <jordo189@gmail.com>
2020-09-07 16:31:34 -0400

    added NFL for DraftKings

--- SportsbookAggregation/SportsBooks/DraftKingsSportsBook.cs
+++ SportsbookAggregation/SportsBooks/DraftKingsSportsBook.cs
@@ -11,9 +11,12 @@ namespace SportsbookAggregation.SportsBooks
     {
         private const string NbaRequestUrl =
             "https://sportsbook.draftkings.com//sites/US-SB/api/v1/eventgroup/103/full?includePromotions=true&format=json";
+        private const string NflRequestUrl =
+            "https://sportsbook.draftkings.com//sites/US-SB/api/v1/eventgroup/3/full?includePromotions=true&format=json";
 
         public IEnumerable<GameOffering> AggregateFutureOfferings()
         {
+            var result = GetFootballOfferings();
             return GetBasketballOfferings();
         }
 
@@ -73,6 +76,72 @@ namespace SportsbookAggregation.SportsBooks
             }
 
             if(totalPointsJson != null)
+            {
+                gameOffering.CurrentOverUnder = totalPointsJson.outcomes[0].line.Value;
+                gameOffering.OverPayOut = Convert.ToInt32(totalPointsJson.outcomes[0].oddsAmerican.Value);
+                gameOffering.UnderPayout = Convert.ToInt32(totalPointsJson.outcomes[1].oddsAmerican.Value);
+            }
+
+            return gameOffering;
+        }
+
+
+        private IEnumerable<GameOffering> GetFootballOfferings()
+        {
+            return GetNflGameOfferings();
+        }
+
+        private static IEnumerable<GameOffering> GetNflGameOfferings()
+        {
+            var nflRequestJson =
+                JsonConvert.DeserializeObject<dynamic>(Program.HttpClient.GetStringAsync(NflRequestUrl).Result);
+
+            var nflGameEvents = ((IEnumerable)nflRequestJson.eventGroup.events).Cast<dynamic>();
+            var nflGameLinesJson = ((IEnumerable)nflRequestJson.eventGroup.offerCategories).Cast<dynamic>().First(g => g.name == "Game Lines");
+            var nflGamesJson = ((IEnumerable)nflGameLinesJson.offerSubcategoryDescriptors).Cast<dynamic>().First(g => g.name == "Game");
+            var nflGameOfferingsJson = ((IEnumerable)nflGamesJson.offerSubcategory.offers).Cast<dynamic>();
+            var gameOfferings = new List<GameOffering>();
+            foreach (var nflGameOfferingJson in nflGameOfferingsJson)
+            {
+                var eventId = ((IEnumerable)nflGameOfferingJson).Cast<dynamic>().First().providerEventId.Value;
+                var nflGameEventJson = ((IEnumerable)nflGameEvents).Cast<dynamic>().First(g => g.providerEventId == eventId);
+
+                gameOfferings.Add(ParseNflGameOffering(nflGameOfferingJson, nflGameEventJson));
+            }
+
+            return gameOfferings;
+        }
+
+        private static GameOffering ParseNflGameOffering(dynamic gameJson, dynamic detailJson)
+        {
+            var teams = detailJson.name.Value.Split('@');
+            var gameOffering = new GameOffering
+            {
+                Site = "DraftKings",
+                Sport = detailJson.eventGroupName,
+                AwayTeam = teams[0].Trim(),
+                HomeTeam = teams[1].Trim(),
+                DateTime = detailJson.startDate.Value.AddHours(-4)
+            };
+
+            var pointSpreadJson = ((IEnumerable)gameJson).Cast<dynamic>().FirstOrDefault(g => g.label == "Point Spread");
+            var totalPointsJson = ((IEnumerable)gameJson).Cast<dynamic>().FirstOrDefault(g => g.label == "Total Points");
+            var moneylineJson = ((IEnumerable)gameJson).Cast<dynamic>().FirstOrDefault(g => g.label == "Moneyline");
+
+            if (pointSpreadJson != null)
+            {
+                gameOffering.CurrentSpread = pointSpreadJson.outcomes[1].line.Value;
+                gameOffering.HomeSpreadPayout = Convert.ToInt32(pointSpreadJson.outcomes[1].oddsAmerican.Value);
+                gameOffering.AwaySpreadPayout = Convert.ToInt32(pointSpreadJson.outcomes[0].oddsAmerican.Value);
+            }
+
+            if (moneylineJson != null)
+            {
+                gameOffering.HomeMoneyLinePayout = Convert.ToInt32(moneylineJson.outcomes[1].oddsAmerican.Value);
+                gameOffering.AwayMoneyLinePayout = Convert.ToInt32(moneylineJson.outcomes[0].oddsAmerican.Value);
+            }
+
+            if (totalPointsJson != null)
             {
                 gameOffering.CurrentOverUnder = totalPointsJson.outcomes[0].line.Value;
                 gameOffering.OverPayOut = Convert.ToInt32(totalPointsJson.outcomes[0].oddsAmerican.Value);
