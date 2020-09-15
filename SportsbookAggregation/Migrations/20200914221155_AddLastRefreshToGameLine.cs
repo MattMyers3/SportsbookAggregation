b0c049d3330152339ce7b226b56a595c7194e933
Matthew Myers <mmyers12@villanova.edu>
2020-09-14 20:33:45 -0400

    Adding Last Refresh Time to Game Line

---
+++ SportsbookAggregation/Migrations/20200914221155_AddLastRefreshToGameLine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportsbookAggregation.Migrations
+{
+    public partial class AddLastRefreshToGameLine : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastRefresh",
+                table: "GameLine",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastRefresh",
+                table: "GameLine");
+        }
+    }
+}
