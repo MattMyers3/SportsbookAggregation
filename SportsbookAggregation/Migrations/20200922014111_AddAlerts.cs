147458e722b93eb016fa55c93bb9852de2555651
NSmizzy <nismith@udel.edu>
2020-09-21 21:44:14 -0400

    Alerts service

---
+++ SportsbookAggregation/Migrations/20200922014111_AddAlerts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportsbookAggregation.Migrations
+{
+    public partial class AddAlerts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alert",
+                columns: table => new
+                {
+                    AlertId = table.Column<Guid>(nullable: false),
+                    GameId = table.Column<Guid>(nullable: false),
+                    Type = table.Column<string>(nullable: true),
+                    CurrentOver = table.Column<double>(nullable: true),
+                    CurrentUnder = table.Column<double>(nullable: true),
+                    CurrentOverPayout = table.Column<int>(nullable: true),
+                    CurrentUnderPayout = table.Column<int>(nullable: true),
+                    CurrentHomeSpread = table.Column<double>(nullable: true),
+                    CurrentHomeSpreadPayout = table.Column<int>(nullable: true),
+                    CurrentAwaySpread = table.Column<double>(nullable: true),
+                    CurrentAwaySpreadPayout = table.Column<int>(nullable: true),
+                    CurrentHomeMoneyLine = table.Column<int>(nullable: true),
+                    CurrentAwayMoneyLine = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alert", x => x.AlertId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alert");
+        }
+    }
+}
--- SportsbookAggregation/Program.cs
+++ SportsbookAggregation/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Mail;
+using SportsbookAggregation.Alerts;
 using SportsbookAggregation.Data;
 using SportsbookAggregation.SportsBooks;
 using SportsbookAggregation.SportsBooks.Models;
@@ -17,31 +19,33 @@ namespace SportsbookAggregation
         {
             var dbContext = new Context();
 
-            List<ISportsBook> sportsbooks = new List<ISportsBook> { new DraftKingsSportsBook(), new FanDuelSportsBook(), new FoxBetSportsBook(), new BarstoolSportsBook(), new BetAmericaSportsBook(), new CaesarsSportBook(), new BetRiversSportsBook(), new ParxSportsBook(), new UnibetSportsBook(), new SugarHouseSportsBook()};
-            var gameOfferings = new List<GameOffering>();
-            foreach(var sportsbook in sportsbooks)
-            {
-                try
-                {
-                    gameOfferings.AddRange(sportsbook.AggregateFutureOfferings().ToList());               
-                }
-                catch(Exception ex)
-                {
-                    LogError(ex);
-                    Console.WriteLine("Failed to Parse: " + sportsbook.GetSportsBookName());
-                }
-            }
+            //List<ISportsBook> sportsbooks = new List<ISportsBook> { new DraftKingsSportsBook(),  new FoxBetSportsBook(), new BarstoolSportsBook(), new BetAmericaSportsBook(), new CaesarsSportBook(), new BetRiversSportsBook(), new ParxSportsBook(), new UnibetSportsBook(), new SugarHouseSportsBook() };
+            //// List<ISportsBook> sportsbooks = new List<ISportsBook> { new FanDuelSportsBook() };
+            //var gameOfferings = new List<GameOffering>();
+            //foreach (var sportsbook in sportsbooks)
+            //{
+            //    try
+            //    {
+            //        gameOfferings.AddRange(sportsbook.AggregateFutureOfferings().ToList());
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        LogError(ex);
+            //        Console.WriteLine("Failed to Parse: " + sportsbook.GetSportsBookName());
+            //    }
+            //}
 
-            try
-            {
-                var databaseUpdater = new SportsbookOfferingsUpdater(dbContext);
-                databaseUpdater.WriteGameOfferings(gameOfferings);
-            }
-            catch(Exception ex)
-            {
-                LogError(ex);
-                throw ex;
-            }
+            //try
+            //{
+            //    var databaseUpdater = new SportsbookOfferingsUpdater(dbContext);
+            //    databaseUpdater.WriteGameOfferings(gameOfferings);
+            //}
+            //catch (Exception ex)
+            //{
+            //    LogError(ex);
+            //    throw ex;
+            //}
+            AlertsService.Run(dbContext);
         }
 
         public static void LogError(Exception ex)
