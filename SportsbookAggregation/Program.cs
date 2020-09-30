3e919a83c31e19d2e0bae6e1ee08c72726b74dc1
NSmizzy <nismith@udel.edu>
2020-09-29 20:17:06 -0400

    uncomment books

--- SportsbookAggregation/Program.cs
+++ SportsbookAggregation/Program.cs
@@ -19,8 +19,7 @@ namespace SportsbookAggregation
         {
             var dbContext = new Context();
 
-            // List<ISportsBook> sportsbooks = new List<ISportsBook> { new DraftKingsSportsBook(), new FanDuelSportsBook(), new FoxBetSportsBook(), new BarstoolSportsBook(), new BetAmericaSportsBook(), new CaesarsSportBook(), new BetRiversSportsBook(), new ParxSportsBook(), new UnibetSportsBook(), new SugarHouseSportsBook() };
-            List<ISportsBook> sportsbooks = new List<ISportsBook> { new DraftKingsSportsBook() };
+            List<ISportsBook> sportsbooks = new List<ISportsBook> { new DraftKingsSportsBook(), new FanDuelSportsBook(), new FoxBetSportsBook(), new BarstoolSportsBook(), new BetAmericaSportsBook(), new CaesarsSportBook(), new BetRiversSportsBook(), new ParxSportsBook(), new UnibetSportsBook(), new SugarHouseSportsBook() };
             var gameOfferings = new List<GameOffering>();
             foreach (var sportsbook in sportsbooks)
             {
