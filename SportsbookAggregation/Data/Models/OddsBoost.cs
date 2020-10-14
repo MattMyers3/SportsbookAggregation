ebf0bb2681e955101f8fa03c031d99c098ba80a3
Matthew Myers <mmyers12@villanova.edu>
2020-10-13 20:00:04 -0400

    Updating OddsBoost Table

--- SportsbookAggregation/Data/Models/OddsBoost.cs
+++ SportsbookAggregation/Data/Models/OddsBoost.cs
@@ -4,16 +4,19 @@ namespace SportsbookAggregation.Data.Models
 {
     public class OddsBoost
     {
-            public Guid OddsBoostId { get; set; }
-            public string Description { get; set; }
-            public int PreviousOdds { get; set; }
-            public int BoostedOdds { get; set; }
+        public Guid OddsBoostId { get; set; }
+        public string Description { get; set; }
+        public int PreviousOdds { get; set; }
+        public int BoostedOdds { get; set; }
 
-            public Sport Sport { get; set; }
-            public Guid SportId { get; set; }
+        public Sport Sport { get; set; }
+        public Guid SportId { get; set; }
 
-            public GamblingSite GamblingSite { get; set; }
-            public Guid GamblingSiteId { get; set; }
+        public GamblingSite GamblingSite { get; set; }
+        public Guid GamblingSiteId { get; set; }
+
+        public bool IsAvailable { get; set; }
+        public DateTime Date { get; set; }
     }
 }
 
