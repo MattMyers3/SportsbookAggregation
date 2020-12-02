12197fe9f45b58de4e373e8c3bcce367a9759fa9
NSmizzy <nismith@udel.edu>
2020-12-01 21:18:41 -0500

    Add user settings

---
+++ SportsbookAggregation/Migrations/20201124210048_AddUserSettings.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportsbookAggregation.Migrations
+{
+    public partial class AddUserSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserSettings",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    DefaultBooks = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSettings", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserSettings");
+        }
+    }
+}
---
+++ SportsbookAggregation/UserSettingsInfo/Models/UserSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsbookAggregation.UserSettingsInfo.Models
+{
+    public class UserSettings
+    {
+        [Key]
+        public string UserId { get; set; }
+
+        public string[] DefaultBooks { get; set; }
+    }
+}
